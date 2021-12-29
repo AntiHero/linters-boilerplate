54aacd1378eeddcdd605a3797a7dcb712d4d126b
Aleksandr Schemelev <prophreaker@gmail.com>
2021-12-29 16:34:16 +0300

    feat: add typescript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['airbnb-base', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 13,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	overrides: [
+		{
+			files: '**/*.+(ts|tsx)',
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				project: 'tsconfig.json',
+				plugins: ['@typescript-eslint'],
+				extends: ['plugin:@typescript-eslint/recommended'],
+			},
+		},
+	],
+	rules: {},
 };
